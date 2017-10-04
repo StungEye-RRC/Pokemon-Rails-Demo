7679ea0fad38361408cd594d61df012dfb458f91
Kyle Geske <stungeye@gmail.com>
2017-10-04 14:49:31 +0000

    The index action is now loading all pokemon and the count is being displayed in the view.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,12 @@
 class HomeController < ApplicationController
+
+  # This is the index action
+  def index
+    @pokemon = Pokemon.all
+  end
+  # After this action is executed the associated
+  # view will be loaded.
+  # /app/views/home/index.html.erb
+  # Instance variables (@vars) within actions
+  # are automagically shared with the view.
 end
